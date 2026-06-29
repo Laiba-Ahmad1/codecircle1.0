db9d4d44c72e33c067ecdd1653287ce085a7f592
Laiba-Ahmad1 <roblox.engineers@gmail.com>
2026-06-29 13:53:40 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { HoverCard, HoverCardContent } from "@/components/ui/hover-card";
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { Slider } from "@/components/ui/slider";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-game m-0.5 text-2xl">home Page</h1>
+      <Button className="">SUBSCRIBE</Button>
+      
+      
+    
+    </div>
+  );
+}
